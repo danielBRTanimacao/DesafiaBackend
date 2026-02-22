a625bb7c3946988236adda111c13c9aee5dbe452
danielBRTanimacao <danieltenorio2046@gmail.com>
2026-02-22 11:17:57 -0300

    BUILDING FILES

---
+++ src/main/java/daniel/Desafia/DesafiaApplication.java
@@ -0,0 +1,13 @@
+package daniel.Desafia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/daniel/Desafia/DesafiaApplicationTests.java
@@ -0,0 +1,13 @@
+package daniel.Desafia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
