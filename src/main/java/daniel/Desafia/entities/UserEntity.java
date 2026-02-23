b4f414c96cc8d625568ff4edf2b4a47eefb51ee7
danielBRTanimacao <danieltenorio2046@gmail.com>
2026-02-23 09:42:39 -0300

    feat(entity): add user pre complete columns

--- src/main/java/daniel/Desafia/entities/UserEntity.java
+++ src/main/java/daniel/Desafia/entities/UserEntity.java
@@ -1,4 +1,28 @@
 package daniel.Desafia.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
 public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private UUID id;
+
+    private String username;
+    private String email;
+    private String password;
+
+    private String avatarUrl;
+    private String bio;
+    private int xp;
+    private int level;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
