c5c7bb0ae1ecc55100796d6f08cde03dcc811f3e
danielBRTanimacao <danieltenorio2046@gmail.com>
2026-02-23 18:55:33 -0300

    feat(repository): created basic repo

---
+++ src/main/java/daniel/Desafia/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package daniel.Desafia.repositories;
+
+import daniel.Desafia.entities.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<UserEntity, UUID> {
+}
