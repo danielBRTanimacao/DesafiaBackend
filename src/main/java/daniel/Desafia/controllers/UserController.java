ec9d130d2c91cc8fda896e28121b08ab697689f9
danielBRTanimacao <danieltenorio2046@gmail.com>
2026-02-25 17:53:15 -0300

    feat(controller) user

---
+++ src/main/java/daniel/Desafia/controllers/UserController.java
@@ -0,0 +1,4 @@
+package daniel.Desafia.controllers;
+
+public interface UserController {
+}
---
+++ src/main/java/daniel/Desafia/controllers/users/UserControllerImpl.java
@@ -0,0 +1,6 @@
+package daniel.Desafia.controllers.users;
+
+import daniel.Desafia.entities.UserEntity;
+
+public class UserControllerImpl extends UserEntity {
+}
