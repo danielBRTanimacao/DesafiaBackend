a4b38651d461f4c7a721a69e15ee9daba5912312
danielBRTanimacao <danieltenorio2046@gmail.com>
2026-02-26 16:03:19 -0300

    feat(controller) making pageable

--- src/main/java/daniel/Desafia/controllers/categories/CategoryController.java
+++ src/main/java/daniel/Desafia/controllers/categories/CategoryController.java
@@ -1,4 +1,15 @@
 package daniel.Desafia.controllers.categories;
 
+import daniel.Desafia.entities.CategoryEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RequestMapping("api/categories")
 public interface CategoryController {
+    ResponseEntity<Page<CategoryEntity>> getAllCategories(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size
+    );
 }
--- src/main/java/daniel/Desafia/controllers/categories/CategoryControllerImpl.java
+++ src/main/java/daniel/Desafia/controllers/categories/CategoryControllerImpl.java
@@ -1,4 +1,7 @@
 package daniel.Desafia.controllers.categories;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CategoryControllerImpl implements CategoryController {
 }
