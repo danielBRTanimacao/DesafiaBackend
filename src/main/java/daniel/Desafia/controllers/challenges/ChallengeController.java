78a9e6dabc6f375ef3702f4b29fc7840ac925013
danielBRTanimacao <danieltenorio2046@gmail.com>
2026-03-03 17:10:23 -0300

    feat(challenge) updating challenge controll

--- src/main/java/daniel/Desafia/controllers/challenges/ChallengeController.java
+++ src/main/java/daniel/Desafia/controllers/challenges/ChallengeController.java
@@ -1,4 +1,7 @@
 package daniel.Desafia.controllers.challenges;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("api/challenges")
 public interface ChallengeController {
 }
--- src/main/java/daniel/Desafia/controllers/challenges/ChallengeControllerImpl.java
+++ src/main/java/daniel/Desafia/controllers/challenges/ChallengeControllerImpl.java
@@ -1,4 +1,7 @@
 package daniel.Desafia.controllers.challenges;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ChallengeControllerImpl implements ChallengeController {
 }
