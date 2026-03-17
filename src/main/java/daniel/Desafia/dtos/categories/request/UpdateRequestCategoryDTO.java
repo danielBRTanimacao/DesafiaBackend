e6aac45a2e01c762c2979850eb694c650f80bceb
danielBRTanimacao <danieltenorio2046@gmail.com>
2026-03-17 13:36:58 -0300

    feat updating DTO

--- src/main/java/daniel/Desafia/dtos/categories/request/UpdateRequestCategoryDTO.java
+++ src/main/java/daniel/Desafia/dtos/categories/request/UpdateRequestCategoryDTO.java
@@ -1,6 +1,9 @@
 package daniel.Desafia.dtos.categories.request;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record UpdateRequestCategoryDTO(
+        @NotBlank
         Long id,
         String title,
         byte[] icon
--- src/main/java/daniel/Desafia/dtos/challenges/request/CreateRequestChallengeDTO.java
+++ src/main/java/daniel/Desafia/dtos/challenges/request/CreateRequestChallengeDTO.java
@@ -3,18 +3,23 @@ package daniel.Desafia.dtos.challenges.request;
 
 import daniel.Desafia.enums.DifficultyEnum;
 import daniel.Desafia.enums.StatusEnum;
+import jakarta.validation.constraints.NotBlank;
 
 import java.time.LocalDateTime;
 
 public record CreateRequestChallengeDTO(
+        @NotBlank
         Long category_id,
+        @NotBlank
         String title,
         String description,
         DifficultyEnum difficulty,
+        @NotBlank
         int xpReward,
         byte[] image,
         StatusEnum status,
+        @NotBlank
         LocalDateTime startDate,
+        @NotBlank
         LocalDateTime endDate
-) {
-}
+) {}
