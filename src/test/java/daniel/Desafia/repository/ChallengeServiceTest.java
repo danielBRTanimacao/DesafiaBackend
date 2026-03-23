e0f2b19fb7c96f89169100874cc0a87e10b2cbfa
danielBRTanimacao <danieltenorio2046@gmail.com>
2026-03-23 13:42:18 -0300

    feat(test) find id challenge

--- src/test/java/daniel/Desafia/repository/ChallengeServiceTest.java
+++ src/test/java/daniel/Desafia/repository/ChallengeServiceTest.java
@@ -4,10 +4,15 @@ import daniel.Desafia.repositories.CategoryRepository;
 import daniel.Desafia.repositories.ChallengeRepository;
 import daniel.Desafia.services.categories.CategoryServiceImpl;
 import daniel.Desafia.services.challenges.ChallengeServiceImpl;
+import daniel.Desafia.utils.customs.NotFoundException;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
@@ -21,4 +26,13 @@ public class ChallengeServiceTest {
     private ChallengeRepository repository;
 
     private final Long FAKEID = 9999999L;
+
+    @Test
+    void shouldThrowNotFoundExceptionWhenDeletingNonExistingChallenge() {
+        when(repository.findById(FAKEID)).thenReturn(Optional.empty());
+        assertThrows(NotFoundException.class, () -> service.delChallenge(FAKEID));
+
+        Mockito.verify(repository).findById(FAKEID);
+        Mockito.verify(repository, Mockito.never()).deleteById(FAKEID);
+    }
 }
