0d8b01a2e7ca5f0b52c6f733e8f5958ba00abb16
danielBRTanimacao <danieltenorio2046@gmail.com>
2026-03-23 13:39:44 -0300

    feat(test) re-using category tests

--- src/test/java/daniel/Desafia/repository/ChallengeServiceTest.java
+++ src/test/java/daniel/Desafia/repository/ChallengeServiceTest.java
@@ -1,4 +1,24 @@
 package daniel.Desafia.repository;
 
+import daniel.Desafia.repositories.CategoryRepository;
+import daniel.Desafia.repositories.ChallengeRepository;
+import daniel.Desafia.services.categories.CategoryServiceImpl;
+import daniel.Desafia.services.challenges.ChallengeServiceImpl;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class ChallengeServiceTest {
+    @InjectMocks
+    private ChallengeServiceImpl service;
+
+    @Mock
+    private ChallengeRepository repository;
+
+    private final Long FAKEID = 9999999L;
 }
