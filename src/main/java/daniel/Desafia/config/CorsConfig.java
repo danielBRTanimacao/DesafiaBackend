6dec585af654083525b2f264072c6cae19d7e175
danielBRTanimacao <danieltenorio2046@gmail.com>
2026-03-25 13:55:26 -0300

    feat base complete config

--- src/main/java/daniel/Desafia/config/CorsConfig.java
+++ src/main/java/daniel/Desafia/config/CorsConfig.java
@@ -2,10 +2,19 @@ package daniel.Desafia.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
     @Value("${spring.domain.url}")
     private String[] domainUrls;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(domainUrls)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD")
+                .allowedHeaders("*");
+    }
 }
