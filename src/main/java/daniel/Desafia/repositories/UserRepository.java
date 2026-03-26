180eafb2d3ada91f3c9526d2b5f973d0db866ad9
danielBRTanimacao <danieltenorio2046@gmail.com>
2026-03-26 13:40:49 -0300

    feat add repository data

--- src/main/java/daniel/Desafia/repositories/UserRepository.java
+++ src/main/java/daniel/Desafia/repositories/UserRepository.java
@@ -3,7 +3,10 @@ package daniel.Desafia.repositories;
 import daniel.Desafia.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<UserEntity, UUID> {
+    Optional<UserEntity> findByUsername(String username);
 }
