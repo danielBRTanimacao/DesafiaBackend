a04405410512be743eba32ca00ca9c070409b18a
danielBRTanimacao <danieltenorio2046@gmail.com>
2026-03-26 13:40:59 -0300

    removing commits

--- src/main/java/daniel/Desafia/repositories/UserRepository.java
+++ src/main/java/daniel/Desafia/repositories/UserRepository.java
@@ -3,7 +3,6 @@ package daniel.Desafia.repositories;
 import daniel.Desafia.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
