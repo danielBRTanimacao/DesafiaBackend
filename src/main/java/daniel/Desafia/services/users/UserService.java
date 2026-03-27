fa85e24c589623e5993745d951051c8c0fb63571
danielBRTanimacao <danieltenorio2046@gmail.com>
2026-03-27 12:23:46 -0300

    feat basic login

--- src/main/java/daniel/Desafia/services/users/UserService.java
+++ src/main/java/daniel/Desafia/services/users/UserService.java
@@ -1,4 +1,8 @@
 package daniel.Desafia.services.users;
 
+import daniel.Desafia.dtos.users.request.LoginRequestUserDTO;
+import daniel.Desafia.dtos.users.response.LoginResponseUserDTO;
+
 public interface UserService {
+    LoginResponseUserDTO login(LoginRequestUserDTO data);
 }
--- src/main/java/daniel/Desafia/services/users/UserServiceImpl.java
+++ src/main/java/daniel/Desafia/services/users/UserServiceImpl.java
@@ -1,7 +1,13 @@
 package daniel.Desafia.services.users;
 
+import daniel.Desafia.dtos.users.request.LoginRequestUserDTO;
+import daniel.Desafia.dtos.users.response.LoginResponseUserDTO;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
+    @Override
+    public LoginResponseUserDTO login(LoginRequestUserDTO data) {
+        return null;
+    }
 }
