bd328afbd70d32769a47cf0529a79c9dcb0852fe
danielBRTanimacao <danieltenorio2046@gmail.com>
2026-03-28 08:39:58 -0300

    feat add user basic controll

--- src/main/java/daniel/Desafia/services/users/UserService.java
+++ src/main/java/daniel/Desafia/services/users/UserService.java
@@ -1,8 +1,12 @@
 package daniel.Desafia.services.users;
 
+import daniel.Desafia.dtos.users.request.CreateRequestUserDTO;
 import daniel.Desafia.dtos.users.request.LoginRequestUserDTO;
 import daniel.Desafia.dtos.users.response.LoginResponseUserDTO;
 
 public interface UserService {
     LoginResponseUserDTO login(LoginRequestUserDTO data);
+    void createUser(CreateRequestUserDTO data);
+    void updateUser();
+
 }
